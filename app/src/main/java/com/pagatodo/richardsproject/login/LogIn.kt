9cc99d94857f8c9cec418ab5e96f9cd6a5441c26
Omar Zúñiga <ozunigag@pagatodo.com>
2018-11-26 16:47:39 -0600

    * Cambio de extención de clases

---
+++ app/src/main/java/com/pagatodo/richardsproject/login/LogIn.kt
@@ -0,0 +1,13 @@
+package com.pagatodo.richardsproject.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pagatodo.richardsproject.R
+
+class LogIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
---
+++ app/src/main/java/com/pagatodo/richardsproject/register/Register.kt
@@ -0,0 +1,13 @@
+package com.pagatodo.richardsproject.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pagatodo.richardsproject.R
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
