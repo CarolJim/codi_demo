a0699b11cf14da3678d3bc51180c647c34cee836
Omar Zúñiga <ozunigag@pagatodo.com>
2018-11-28 20:15:43 -0600

    * Parceo de JSON Bancos

---
+++ app/src/main/java/com/pagatodo/richardsproject/dtos/Banks.kt
@@ -0,0 +1,6 @@
+package com.pagatodo.richardsproject.dtos
+
+class Banks {
+    var id : Int = 0
+    lateinit var banco : String
+}
--- app/src/main/java/com/pagatodo/richardsproject/modules/send_money/SendMoney.kt
+++ app/src/main/java/com/pagatodo/richardsproject/modules/send_money/SendMoney.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import androidx.databinding.DataBindingUtil
+import com.google.gson.Gson
+import com.google.gson.JsonArray
 import com.pagatodo.richardsproject.R
+import com.pagatodo.richardsproject.commons.Constants
 import com.pagatodo.richardsproject.databinding.ActivitySendMoneyBinding
+import com.pagatodo.richardsproject.dtos.Banks
 import com.pagatodo.richardsproject.watchers.CardTextWatcher
 import com.pagatodo.richardsproject.watchers.AmountTextWatcher
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class SendMoney : AppCompatActivity(), SendMoneyContracts.Presenter {
 
@@ -30,5 +37,16 @@ class SendMoney : AppCompatActivity(), SendMoneyContracts.Presenter {
         }
         var cardTextWatcher = CardTextWatcher(binding.edtCardNumber, 19)
         binding.edtCardNumber.addTextChangedListener(cardTextWatcher)
+        /* Bank json to object */
+        val jsonArray = JSONArray(Constants().BANKS_JSON)
+        val list = ArrayList<Banks>()
+        val strings = ArrayList<String>()
+        for (i in 0..jsonArray.length() step 1) {
+            var bank = Banks()
+            bank.id = jsonArray.getJSONObject(i).getInt("id")
+            bank.banco = jsonArray.getJSONObject(i).getString("banco")
+            strings.add(bank.banco)
+            list.add(bank)
+        }
     }
 }
